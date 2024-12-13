ffb3110200032b7a8dbdd561d385668b9994c94e
Marais Devis <maraisdcdevis@gmail.com>
2024-12-13 14:31:30 +0100

    Start basic layout of land. pg and bio page

---
+++ app/bio/page.js
@@ -0,0 +1,103 @@
+export const metadata = {
+  title: 'About',
+  description: 'Alpha Deficit Band Page',
+};
+
+export default function BioPage() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <br />
+      <div>
+        <h2>Place holder for picture</h2>
+      </div>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et
+      </p>
+      <br />
+      <div>
+        <h2>Members</h2>
+        <br />
+        <div>
+          <h3>Member1 name</h3>
+          <h2>placeholder for picture of band member</h2>
+          <h3>Instrument: vocals/guitar/drums</h3>
+          <h4>Bio:</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua.
+          </p>
+        </div>
+        <br />
+        <div>
+          <h3>Member2 name</h3>
+          <h2>placeholder for picture of band member</h2>
+          <h3>Instrument: vocals/guitar/drums</h3>
+          <h4>Bio:</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua.
+          </p>
+        </div>
+        <br />
+        <div>
+          <h3>Member3 name</h3>
+          <h2>placeholder for picture of band member</h2>
+          <h3>Instrument: vocals/guitar/drums</h3>
+          <h4>Bio:</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua.
+          </p>
+        </div>
+        <br />
+        <div>
+          <h3>Member4 name</h3>
+          <h2>placeholder for picture of band member</h2>
+          <h3>Instrument: vocals/guitar/drums</h3>
+          <h4>Bio:</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua.
+          </p>
+        </div>
+        <br />
+        <div>
+          <h3>Member5 name</h3>
+          <h2>placeholder for picture of band member</h2>
+          <h3>Instrument: vocals/guitar/drums</h3>
+          <h4>Bio:</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua.
+          </p>
+        </div>
+        <br />
+        <div>
+          <h3>Member6 name</h3>
+          <h2>placeholder for picture of band member</h2>
+          <h3>Instrument: vocals/guitar/drums</h3>
+          <h4>Bio:</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua.
+          </p>
+        </div>
+        <br />
+      </div>
+    </div>
+  );
+}
