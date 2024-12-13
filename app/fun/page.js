eb6769f699766b20875e89894869273dd2fcb93d
Marais Devis <maraisdcdevis@gmail.com>
2024-12-13 15:41:39 +0100

    Add shows and the potential 'fun' page

---
+++ app/fun/page.js
@@ -0,0 +1,17 @@
+export const metadata = {
+  title: 'Play/Sing/Watch Along',
+  description: 'Alpha Deficit Band Page',
+};
+
+export default function FunPage() {
+  return (
+    <div>
+      <h2>Come along with us for some fun!</h2>
+      <section>
+        Since you said this is not certain I am not going to put anything major
+        here until you figure out if you want it or not, just figured I'd have
+        it ready to go in case you do ;-).
+      </section>
+    </div>
+  );
+}
---
+++ app/shows/page.js
@@ -0,0 +1,20 @@
+export const metadata = {
+  title: 'Gigs',
+  description: 'Alpha Deficit Band Page',
+};
+
+export default function ShowsPage() {
+  return (
+    <div>
+      <h2>Come to our Gigs!</h2>
+      <div>
+        <h3>Upcoming Shows!</h3>
+        <section>List of upcoming shows and links to the details</section>
+      </div>
+      <div>
+        <h3>Past Shows</h3>
+        <section>List of past shows and links to the details</section>
+      </div>
+    </div>
+  );
+}
